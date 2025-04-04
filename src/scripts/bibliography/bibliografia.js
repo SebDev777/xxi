ef39ac9c84732d6131bcaa99ff4f82cb4883e2fb
sebdev677 <sebastiangamers17@gmail.com>
2025-04-04 15:55:24 -0500

    Ultimos arreglos

---
+++ src/scripts/bibliography/bibliografia.js
@@ -0,0 +1,18 @@
+export const lista = [
+    {
+        name: "Los personajes importantes en el siglo XXI",
+        url: "https://www.muyinteresante.com/ciencia/61337.html"
+    },
+    {
+        name: "Avances en el siglo XXI",
+        url: "https://alfaiot.com/actualidad-iot/avances-tecnologicos-que-han-transformado-el-siglo-xxi/"
+    },
+    {
+        name: "Exposición en el siglo XXI",
+        url: "https://www.canva.com/design/DAGhLAIwKfI/emU64q6DIUc1_4q1uoO4jg/edit?utm_content=DAGhLAIwKfI&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton"
+    },
+    {
+        name: "Inteligencia Artificial – ChatGPT (OpenAI)",
+        url: "https://openai.com/chatgpt/overview/"
+    },
+]
